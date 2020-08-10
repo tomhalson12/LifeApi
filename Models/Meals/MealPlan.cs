5ecdb1f2cd2136d74ccf0ef651ffe05186dac11f
tomhalson12 <tom.halson@hotmail.co.uk>
2020-08-10 15:37:04 +0100

    meal plan class and repository

---
+++ Models/Meals/MealPlan.cs
@@ -0,0 +1,23 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+using System;
+
+namespace LifeApi.Models
+{
+    public class MealPlan
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+
+        public ICollection<string> mealIds { get; set; }
+
+        public string previousPlanId { get; set; }
+
+        public int sequenceNumber { get; set; }
+
+        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
+        public DateTime startDate { get; set; }
+    }
+}
